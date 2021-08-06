8850fea415aa7da9319ce24ac6c287a169ce32c7
Christian Dienemann <cdienem@users.noreply.github.com>
2021-08-06 11:29:27 +0200

    Add some more explanatory comments and absolute/relative chioce for non-dry runs

--- gentleCleaner.py
+++ gentleCleaner.py
@@ -3,6 +3,8 @@ import os, sys, re, glob
 from os.path import join, islink
 
 def isRelionProjectDirectory(location):
+# Checks if a directory is a Relion project by looking for a default_pipeline.star
+# Beware, this can also be true for job folders! -> see readRelionProjectJobs
 	files = os.listdir(location)
 	#print(files)
 	if "default_pipeline.star" in files:
@@ -38,8 +40,8 @@ def readRelionProjectJobs(location):
 					# Also check whether job directories exist
 					if line.split()[3] == "2" and os.path.isdir(join(location,line.split()[0])):
 						jobs.append(line.split()[0])
-	print(jobs)
-	return(jobs)
+	#print(jobs)
+	return jobs
 
 def readRelionProjectNodes(location):
 # Quick and dirty retrieval of nodes from pipeline star file, is needed for cleaning 2D etc.
@@ -65,26 +67,27 @@ def readRelionProjectNodes(location):
 					break
 				else:
 					nodes.append(line.split()[0])
-	return(nodes)
+	return nodes
 
 
 
 def cleanRelionJobFiles(location, job, harsh=False, dry=True, absolute=True):
-# Location: place of the relion project
+# location: place of the relion project
 # job: Job folder like Class2D/jobXX
-# harsh: True = perform harsch cleaning
-# dry (True/False): either print just a file list or actually do clean (e.g. move things to trash)
-# absolute: only when doing dry runs, True=print absolute paths, False=print paths relative to project directory
-# Return: a list of files to be cleaned (or that were clened)
+# harsh: True = perform harsh cleaning
+# dry (True/False): either return just a file list or actually do clean (e.g. move things to trash)
+# absolute: only when doing dry runs, True means print absolute paths, False means print paths relative to project directory
+# Returns a list of files to be cleaned (or that were moved to Trash when harsh = True)
 
 	# Get job type
 	job_type = job.split("/")[0]
 	
-	# TODO: check accurate names!
-	if job_type in ["Import", "Manualpick", "Select", "MaskCreate", "JoinStar", "LocalRes"]:
+	# Do not clean these job types
+	if job_type in ["Import", "ManualPick", "Select", "MaskCreate", "JoinStar", "LocalRes"]:
 		return []
 	
 	#print(job_type)
+	# Initialize delete files
 	delete_files = []
 	
 	
@@ -130,6 +133,7 @@ def cleanRelionJobFiles(location, job, harsh=False, dry=True, absolute=True):
 		# listdir gives unsorted listing, sort the files!
 		files = sorted(os.listdir(join(location,job)))
 
+		# Use a list as it stays ordered, so all iterations except last can be cleaned
 		iteration_files = []
 		iteration_files.extend([join(job,f).replace("_data.star","") for f in files if re.match('run_it([0-9]){3}_data.star', f) != None])
 		iteration_files.extend([join(job,f).replace("_data.star","") for f in files if re.match('run_ct([0-9]){1}_it([0-9]){3}_data.star', f) != None])
@@ -170,7 +174,6 @@ def cleanRelionJobFiles(location, job, harsh=False, dry=True, absolute=True):
 				delete_files.extend([join(root,f) for f in files if re.match('.*_fit\.eps', f) != None])
 
 	elif job_type == "Polish":
-		# TODO: implemet harsh -> remove all *_shiny.star/mrcs files from subfolders
 		for root, dirs, files in os.walk(join(location,job), topdown=True):
 			if root != join(location,job): # skip first level
 				delete_files.extend([join(root,f) for f in files if re.match('.*_FCC_cc\.mrc', f) != None])
@@ -199,7 +202,7 @@ def cleanRelionJobFiles(location, job, harsh=False, dry=True, absolute=True):
 			# make paths relative by removing location
 			return [f.replace(location+"/","") for f in delete_files]
 	else:
-		# Make delete_files with relative paths
+		# Make delete_files with relative paths for moving them to Trash
 		move_to_trash = [f.replace(location+"/","") for f in delete_files]
 		
 		for file in move_to_trash:
@@ -207,7 +210,11 @@ def cleanRelionJobFiles(location, job, harsh=False, dry=True, absolute=True):
 			file_name = os.path.basename(file)
 			trash_path = join(location,"Trash",os.path.dirname(file))
 			
-			# move
+			# Move
 			os.renames(join(location,file), join(location, trash_path, file_name))
 		
-		return delete_files
+		# Give choice what should be returned
+		if absolute:
+			return delete_files
+		else:
+			return move_to_trash
